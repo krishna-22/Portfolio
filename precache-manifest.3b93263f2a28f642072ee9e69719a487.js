d95f59120ab7d0547535c25674c476e067dd9e02
krishna-22 <goginenisaikiran31677@gmail.com>
2020-04-02 19:47:17 +0530

    Updates

---
+++ precache-manifest.3b93263f2a28f642072ee9e69719a487.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3ca0e0ece295c8e992bee673a8c7cd1",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "4c1c429c65edad2f979a",
+    "url": "/Portfolio/static/css/2.617247dc.chunk.css"
+  },
+  {
+    "revision": "433bc949a470e2f574a8",
+    "url": "/Portfolio/static/css/main.60720732.chunk.css"
+  },
+  {
+    "revision": "4c1c429c65edad2f979a",
+    "url": "/Portfolio/static/js/2.09adee4b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Portfolio/static/js/2.09adee4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "433bc949a470e2f574a8",
+    "url": "/Portfolio/static/js/main.e0a30d39.chunk.js"
+  },
+  {
+    "revision": "0d87ac212d7a5e923864",
+    "url": "/Portfolio/static/js/runtime-main.63ef7960.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
